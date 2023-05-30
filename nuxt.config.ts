324ea09e0f328e5c2165744eb377019ac0fdbe7e
fooooooooooooooo <34515228+fooooooooooooooo@users.noreply.github.com>
2023-05-30 12:03:01 -0700

    config error

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 import MyModule from './my-module';
 
 export default defineNuxtConfig({
+  // Type 'NuxtModule<MyModuleOptions>' is not assignable to type 'string | false | NuxtModule<ModuleOptions> | [string | NuxtModule<ModuleOptions>, Record<string, any>] | [...] | [...] | null | undefined'.
   modules: [MyModule, '@nuxt/content'],
   content: {
     'my-module': {
